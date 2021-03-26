f7764ab0dbd9c75dbeaad85e9d4105f58b155b00
chatgris <chatgri@gmail.com>
2021-03-26 12:39:46 +0900

    Debug
    Signed-off-by: chatgris <julien.boyer@zeals.co.jp>

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -59,7 +59,7 @@ unless windows
   nproc = Etc.nprocessors * 2 if Etc.respond_to? :nprocessors
   make = bsd ? 'gmake' : 'make'
   command = "#{make} -j#{nproc} -C #{grpc_root} #{grpc_lib_dir}/libgrpc.a CONFIG=#{grpc_config} Q="
-  p pwd
+  p Dir.pwd
   p command
   system(command)
   exit 1 unless $? == 0
